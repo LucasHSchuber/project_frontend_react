5b66fcf1a27e3baf52c2e8fe03a6bc98e5d37232
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-03-13 07:32:14 +0100

    change api.js file

--- src/api.js
+++ src/api.js
@@ -3,12 +3,19 @@
 // api.js global
 // export const BASE_URL = 'https://aradalenapi.azurewebsites.net/';
 // api.js local
-export const URL = 'http://localhost:5111'
+
+// export const URL = 'http://localhost:5111'
+// export const AUDIO_ENDPOINT = "api/audioapi";
+// export const USER_ENDPOINT = 'api/userapi';
+// export const AVATAR_ENDPOINT = 'api/avatarapi';
+// export const USERLIST_ENDPOINT = 'api/userlistapi';
+// export const USERAUDIO_ENDPOINT = 'api/useraudioapi';
+// export const LIKE_ENDPOINT = 'api/likeapi';
+
+export const URL = 'https://mindspaceadminapp.azurewebsites.net'
 export const AUDIO_ENDPOINT = "api/audioapi";
 export const USER_ENDPOINT = 'api/userapi';
 export const AVATAR_ENDPOINT = 'api/avatarapi';
 export const USERLIST_ENDPOINT = 'api/userlistapi';
 export const USERAUDIO_ENDPOINT = 'api/useraudioapi';
 export const LIKE_ENDPOINT = 'api/likeapi';
-
-// Add other endpoints as needed
